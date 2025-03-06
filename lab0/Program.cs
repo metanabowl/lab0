9510bfde0dd6e5b808c23a428dee1219749f6a17
student <student@DESKTOP-I3OGHQQ>
2025-03-06 16:02:44 +0100

    Dodaj pliki projektów.

---
+++ lab0/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace lab0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzz fizzbuzz = new FizzBuzz();
+            fizzbuzz.Metoda();
+        }
+    } 
+}
---
+++ lab0/fizzbuzz.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab0
+{
+    internal class FizzBuzz
+    {
+        int upperLimit;
+        public FizzBuzz()
+        {
+            Console.Write("Wpisz gorny limit: ");
+            int upperLimit = int.Parse(Console.ReadLine());
+            this.upperLimit = upperLimit;
+        }
+
+        public void Metoda()
+        {
+            for (int i = 1; i <= upperLimit; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.Write("Fizz");
+                }
+                if (i % 5 == 0)
+                {
+                    Console.Write("Buzz");
+                }
+                if (i % 3 != 0 && i % 5 != 0)
+                {
+                    Console.Write(i);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
